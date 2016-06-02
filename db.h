ddd9dee764fe537d2d4e382521a6e10ca71417b6
Zhang Huanjie <james@ustc.edu.cn>
2016-06-02 10:52:33 +0800

    db.h

---
+++ db.h
@@ -0,0 +1,27 @@
+#define DBUSER "root"
+#define DBPASSWD ""
+#define DB     "adsb"
+#define DBHOST "localhost"
+#define DBPORT 3306
+#define DBSOCKPATH "/var/lib/mysql/mysql.sock"
+
+#include <mysql/mysql.h>
+MYSQL *mysql;
+
+void err_quit(char*s)
+{
+	fprintf(stderr,s);
+	exit(0);
+}
+
+MYSQL * connectdb(void)
+{
+        MYSQL *mysql;
+
+        if ((mysql=mysql_init(NULL))==NULL) 
+                err_quit("mysql_init error\n");
+        if( mysql_real_connect(mysql, DBHOST, DBUSER, DBPASSWD,
+        DB, DBPORT, DBSOCKPATH, 0)== NULL)
+                err_quit("mysql_init error\n");
+        return mysql;
+}
