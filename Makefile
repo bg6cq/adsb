79d5ac8a1abee9dbc96bf1cdef3fa8d1c2179edd
Zhang Huanjie <james@ustc.edu.cn>
2016-06-05 20:02:52 +0800

    send2servers/tcp2servers

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
-all: adsb
+all: adsb send2servers tcp2servers
 
 adsb: adsb.c db.h
 	gcc -g -o adsb adsb.c -lm  -lmysqlclient -L/usr/lib64/mysql/
 
+send2servers: send2servers.c
+	gcc -g -o send2servers send2servers.c
+
+tcp2servers: tcp2servers.c
+	gcc -g -o tcp2servers tcp2servers.c
 
--- send2servers.c
+++ send2servers.c
@@ -1,11 +1,9 @@
 /* send2servers v1.0 by  james@ustc.edu.cn 2016.05.11
-   ./send2servers xxxx a.a.a.a:port b.b.b.b:port c.c.c.c:port
-   accept connection on tcp port xxxx
-	send all data received to
+   ./send2servers a.a.a.a:port b.b.b.b:port c.c.c.c:port
+	send all data readed from stdin to:
 		a.a.a.a:port
 		b.b.b.b:port
 		c.c.c.c:port
-	
 */
 
 #include <stdio.h>
@@ -31,6 +29,7 @@
 
 #define MAXLEN 16384
 #define STRLEN 100
+
 int total_server=0;
 int server_fd[MAXS];
 char server_port[MAXS][STRLEN];
@@ -40,17 +39,8 @@ void Process(int c_fd)
         unsigned char buf[MAXLEN];
         int i,n;
         int optval;
-        socklen_t optlen = sizeof(optval);
-        optval = 120;
-        Setsockopt(c_fd, SOL_SOCKET, SO_KEEPALIVE, &optval, optlen);
-        optval = 10;
-        Setsockopt(c_fd, SOL_TCP, TCP_KEEPCNT, &optval, optlen);
-        optval = 120;
-        Setsockopt(c_fd, SOL_TCP, TCP_KEEPIDLE, &optval, optlen);
-        optval = 10;
-        Setsockopt(c_fd, SOL_TCP, TCP_KEEPINTVL, &optval, optlen);
 
-	for(i=0;i<total_server;i++) {
+	for(i=0; i<total_server; i++) {
 		char *p;
 		p = strchr(server_port[i],':');
 		if(p==NULL) exit(0);	
@@ -76,7 +66,7 @@ void Process(int c_fd)
 #ifdef DEBUG
 		dump_pkt(buf,n);
 #endif
-		for(i=0;i<total_server;i++) {
+		for(i=0; i<total_server; i++) {
 			Write(server_fd[i],buf,n);
 		}
         }
@@ -84,13 +74,12 @@ void Process(int c_fd)
 
 void usage()
 {
-	printf("\nsend2servers v1.0 by james@ustc.edu.cn\n");
+	printf("send2servers v1.0 by james@ustc.edu.cn\n");
 	printf("%s",
-   	"./send2servers xxxx a.a.a.a:port b.b.b.b:port c.c.c.c:port"
-   	"accept connection on tcp port xxxx"
-   	"send all data received"
-	"	a.a.a.a:port"
-	"	b.b.b.b:port"
+	"./send2servers a.a.a.a:port [ b.b.b.b:port c.c.c.c:port ... ] \n"
+	"read from stdin, send all data to\n"
+	"	a.a.a.a:port\n"
+	"	b.b.b.b:port\n"
 	"	c.c.c.c:port\n");
 	printf("max servers: %d\n",MAXS);
 	exit(0);
@@ -104,39 +93,15 @@ int main(int argc, char *argv[])
 	char *port;
 	int i;
 
-	if((argc<3)|(argc-2>MAXS)) 
+	if((argc<2)|(argc-1>MAXS))
 		usage();
 
-	port = argv[1];
-	printf("accepting on port %s\n", port);
-	total_server = argc-2;
-	printf("sending to %d servers\n",total_server);
+	total_server = argc-1;
+	printf("sending stdin to %d servers:\n",total_server);
 	for(i=0;i<total_server;i++) {
-		strncpy(server_port[i],argv[2+i],STRLEN);
+		strncpy(server_port[i],argv[1+i],STRLEN);
 		printf("    %s\n",server_port[i]);
 	}
 		
-	signal(SIGCHLD,SIG_IGN);
-
-#ifndef DEBUG
-	daemon_init("send2servers",LOG_DAEMON);
-#endif
-
-	listen_fd = Tcp_listen("0.0.0.0",port,(socklen_t *)&llen);
-
-	while (1) {
-		struct sockaddr sa; int slen;
-		slen = sizeof(sa);
-		c_fd = Accept(listen_fd, &sa, (socklen_t *)&slen);
-#ifdef DEBUG
-		fprintf(stderr,"get connection:\n");
-		Process(c_fd);
-#else
-		if( Fork()==0 ) {
-			Close(listen_fd);
-			Process(c_fd);
-		}
-#endif
-		Close(c_fd);
-	}
+	Process(0);  // 0 is stdin
 }
